14b55933a4f245aac2ca4d894b134cec3bb61547
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-11-30 21:03:27 -0300

    chore: if have more participantes then presentes, throw exception

--- Controllers/SorteioController.cs
+++ Controllers/SorteioController.cs
@@ -6,27 +6,40 @@ namespace Etec.Sorteio.Controllers;
 
 public class SorteioController
 {
-    private SorteioRepository _repository;
+    private SorteioRepository _sorteioRepository;
+    private ParticipantesRepository _participantesRepository;
+    private PresenteRepository _presenteRepository;
 
-    public SorteioController(SorteioRepository repository) => _repository = repository;
+    public SorteioController(SorteioRepository sorteioRepository, ParticipantesRepository participanteRepository, PresenteRepository presenteRepository)
+    {
+        _presenteRepository = presenteRepository;
+        _sorteioRepository = sorteioRepository;
+        _participantesRepository = participanteRepository;
+    }
 
     public async Task CreateAsync(SorteioModel model)
     {
-        await _repository.CreateAsync(model);
+        await _sorteioRepository.CreateAsync(model);
     }
 
     public async Task DeleteAsync(int id)
     {
-        await _repository.DeleteAsync(id);
+        await _sorteioRepository.DeleteAsync(id);
     }
 
-    public async Task<IEnumerable<SorteioModel>> GetAsync() 
+    public async Task<IEnumerable<SorteioModel>> GetAsync()
     {
-        return await _repository.GetAsync();
+        return await _sorteioRepository.GetAsync();
     }
 
     public async Task MakeRaffle()
     {
-        await _repository.MakeRaffle();
+        var participantes = await _participantesRepository.GetAsync();
+        var presentes = await _presenteRepository.GetAsync();
+
+        if (participantes.Count() != presentes.Count())
+            throw new Exception("Para realizar o sorteio, você precisa ter o mesmo número de participantes e presentes!");
+
+        await _sorteioRepository.MakeRaffle();
     }
 }
